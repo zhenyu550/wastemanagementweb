22bcf04b9e6c572b55d9ffdf9aa32f33b05b4cd8
Fionalisha <57385223+Fionalisha@users.noreply.github.com>
2020-07-02 09:48:24 +0800

    Update collectors.php
    Added canvas.js for all bins from bin table, but fixed on default cp_id = 1.
    Added clear button that will go to collectors_post.php

--- collectors.php
+++ collectors.php
@@ -31,12 +31,230 @@
     <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="css/custom.css">
+    <!-- Canvasjs-->
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.png?3">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
   </head>
+  <?php
+$servername   = "localhost";
+$username   = "root";
+$password   = "";
+$databasename = "wastemanagement";
+
+$DBConn   = new mysqli($servername, $username, $password, $databasename);
+
+$sql = "SELECT * FROM bin WHERE cp_id = 1";
+$result = $DBConn->query($sql);
+$count = mysqli_num_rows($result);
+
+$paper = 0;
+$glass = 0;
+$metal = 0;
+$plastic = 0;
+$fabric = 0;
+$chemical = 0;
+$electric = 0;
+$wood = 0;
+
+while ($row = $result->fetch_assoc()) {
+  $wastetype = $row['type_id'];
+  if ($wastetype == "1") {
+    $paper =  $row['capacity_current'];
+  } elseif ($wastetype == "2") {
+    $glass =  $row['capacity_current'];
+  } elseif ($wastetype == "3") {
+    $metal =  $row['capacity_current'];
+  } elseif ($wastetype == "4") {
+    $plastic =  $row['capacity_current'];
+  } elseif ($wastetype == "5") {
+    $fabric =  $row['capacity_current'];
+  } elseif ($wastetype == "6") {
+    $chemical =  $row['capacity_current'];
+  } elseif ($wastetype == "7") {
+    $electric =  $row['capacity_current'];
+  } elseif ($wastetype == "8") {
+    $wood =  $row['capacity_current'];
+  }
+}
+?>
+<script>
+  window.onload = function() {
+
+    var chart = new CanvasJS.Chart("paperr", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($paper * 100) / 100 ?>,
+            label: "Paper"
+          },
+          {
+            y: <?php echo (100 - ($paper * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("glasss", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($glass * 100) / 100 ?>,
+            label: "Glass"
+          },
+          {
+            y: <?php echo (100 - ($paper * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("metall", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($metal * 100) / 100 ?>,
+            label: "Metal"
+          },
+          {
+            y: <?php echo (100 - ($metal * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("plasticc", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($plastic * 100) / 100 ?>,
+            label: "Plastic"
+          },
+          {
+            y: <?php echo (100 - ($plastic * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("fabricc", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($fabric * 100) / 100 ?>,
+            label: "Fabric"
+          },
+          {
+            y: <?php echo (100 - ($fabric * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("chemicall", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($chemical * 100) / 100 ?>,
+            label: "Chemical"
+          },
+          {
+            y: <?php echo (100 - ($chemical * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("electricc", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($electric * 100) / 100 ?>,
+            label: "Electric & Electronic"
+          },
+          {
+            y: <?php echo (100 - ($electric * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+    var chart = new CanvasJS.Chart("woodd", {
+      animationEnabled: true,
+      data: [{
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [{
+            y: <?php echo ($wood * 100) / 100 ?>,
+            label: "Wood"
+          },
+          {
+            y: <?php echo (100 - ($wood * 100) / 100) ?>,
+            label: "Remaining"
+          },
+        ]
+      }]
+    });
+    chart.render();
+
+  }
+</script>
   <body>
     <!-- navbar-->
     <?php include("header.php"); ?>  
@@ -45,6 +263,128 @@
       <?php include("sidebar.php"); ?> 
       <div class="page-holder w-100 d-flex flex-wrap">
         <div class="container-fluid px-xl-5">
+          <section class="py-5">
+          <div class="row">
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-violet"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Paper</h6><span class="text-gray"><?php echo $paper ?> kg</span>
+                    <br />
+                    <form action="collectors_post.php" method="post">
+                      <input type="submit" class="btn btn-secondary" value="Clear" id="1"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="paperr" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-green"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Glass</h6><span class="text-gray"><?php echo $glass ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="2"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="glasss" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-blue"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Metal</h6><span class="text-gray"><?php echo $metal ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="3"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="metall" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-red"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Plastic</h6><span class="text-gray"><?php echo $plastic ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="4"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="plasticc" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-violet"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Fabric</h6><span class="text-gray"><?php echo $fabric ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="5"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="fabricc" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-green"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Chemical</h6><span class="text-gray"><?php echo $chemical ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="6"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="chemicall" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-blue"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Electric & Electronic</h6><span class="text-gray"><?php echo $electric ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="7"></input>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="electricc" style="height: 300px; width: 100%;"><span class="text-gray"><?php echo $electric ?> kg</span></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="bg-white shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
+                <div class="flex-grow-1 d-flex align-items-center">
+                  <div class="dot mr-3 bg-red"></div>
+                  <div class="text">
+                    <h6 class="mb-0">Wood</h6><span class="text-gray"><?php echo $wood ?> kg</span>
+                    <br /> <input type="submit" class="btn btn-secondary" value="Clear" id="8"></input>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <div id="woodd" style="height: 300px; width: 100%;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
           <section class="py-5">
             <div class="row">
               <div class="col-lg-12 mb-5">
