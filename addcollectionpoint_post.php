230547b80b264aac9cd0933541526e7e4414dd28
zhenyu550 <56509077+zhenyu550@users.noreply.github.com>
2020-07-02 11:46:04 +0800

    Add Collection Point Update
    1. Default bins was added when CP is added.

--- addcollectionpoint_post.php
+++ addcollectionpoint_post.php
@@ -18,6 +18,16 @@ $item->set_fax_no($fax);
 $item->set_social_media_tag($socmed);
 $item->save();
 
+$types = Waste_Type::all();
+foreach($types as $type){
+    $bin = new Bin();
+    $bin->set_cp_id($item->get_id());
+    $bin->set_type_id($type->get_id());
+    $bin->set_capacity_current(0);
+    $bin->set_capacity_max(100);
+    $bin->save();
+}
+
 
 echo '<script language="javascript">';
 echo 'alert("Collection Point Saved.")';
